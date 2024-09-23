49e2b5ec6b96bd6314c345780fb94440f2753b9e
Robert DeRieux <ppldiecomplex@gmail.com>
2024-09-23 11:34:01 -0600

    Render puppy details in a new div of the selected puppy

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,35 @@ import "./App.css";
 function App() {
   console.log(puppyList);
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  const featuredPup = puppies.find((puppy) => puppy.id === featPupId);
+  console.log(`Featured puppy: ${featuredPup}`);
 
   return (
     <>
       <div>
         {puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>;
+          return (
+            <p
+              onClick={() => {
+                setFeatPupId(puppy.id);
+              }}
+              key={puppy.id}
+            >
+              {puppy.name}
+            </p>
+          );
         })}
+        {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
       </div>
     </>
   );
